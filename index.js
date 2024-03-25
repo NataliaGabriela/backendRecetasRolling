30d3ba6bd19801473e239f67eb16c3461b6af746
Natalia Lopez <natigab.24@gmail.com>
2024-03-25 18:29:28 -0300

    cofiguramos una ruta

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import 'dotenv/config'
+import cors from 'cors';
+import morgan from 'morgan';
 const app = express();
 
 app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), ()=>{
     console.log('estoy en el puerto '+ app.get('port'))
+})
+app.use(cors());
+app.use(morgan('dev'))
+
+app.get('/', (req, res)=>{
+    console.log('procesando una solicitud get')
+    res.send('Respuesta del backend rollingRecetas')
 })
